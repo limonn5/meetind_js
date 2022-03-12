755e3a4a2f01181128092bd285f1dd9851670f36
limonn5 <92103116+limonn5@users.noreply.github.com>
2022-03-12 19:29:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// alert('привет')
+
+// нельзя нижнее подчеркивание, и начинать с чисел
+// существует 8 типов данных
+// строка- String
+// числа - Number( есть ограничение)
+// Boolean true(1) and false(0)
+// let age = 14
+// (число можно без кавычек)
+// let itSmoking = false
+// let userName = 'MyLady'
+// console.log('my name - '+userName)
+
+
+// console.log('Im smoking? '+itSmoking)
+
+// // при let можно переименовывать 
+
+// let userName = 'MyLady'
+// console.log('my name - '+userName)
+// userName = 'I'
+// console.log('my name - '+userName)
+
+// // а константу нельзя
+
+// const data = '29.09.2997'
+// console.log ( data)
+
+// let userName = prompt('write the name')
+
+// console.log (userName)
+
+// let age = prompt('Are u 18?', 18)
+
+// console.log (age)
+
+// let itSmoking = confirm('Are u smoking?')
+// console.log(itSmoking)
+
+// let a = Number (prompt('Введіть а'))
+
+// console.log('а = ',a)
+
+// let b = Number (prompt('Введіть b'))
+
+// console.log( 'b = ',b)
+
+// let c = a+b
+
+// console.log('сумма',c)
+
+// необходимо вывести на экран привет мир
+// запросить у пользователа его имя, возраст
+// спросить идет ли сейчас дождь
+// вывести в консоль переименованый узер найме
+
+alert('привет мир')
+let userName = prompt('name')
+console.log ('name' +userName)
+let age = prompt('age')
+console.log('age', age)
+let rain = confirm('Are raining?')
+console.log(rain)
+userName = prompt('name')
+console.log('name' +userName)
+
+
+
